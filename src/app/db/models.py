2551fb95a6b9ad0b87d39abc0c63a59802ba878e
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-09-02 00:25:26 -0300

    feat: atualizando modelo de usuario

--- src/app/db/models.py
+++ src/app/db/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean
 from app.db.base import Base
 
 
 class UserModel(Base):
     __tablename__ = 'users'
-    id = Column('id', Integer, primary_key=True, nullable=False, autoincrement=True)
-    username = Column('username', String, nullable=False, unique=True)
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    email = Column('username', String, nullable=False, unique=True)
     password = Column('password', String, nullable=False)
+    emailVerified = Column('emailVerified', Boolean, default='false')
