798f00b951e1386fd97f4281effecc75190685ce
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-09-06 20:37:13 -0300

    :rocket: novos modelos e relacionamento

--- src/db/models.py
+++ src/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, LargeBinary, ForeignKey
+from sqlalchemy.orm import relationship
 
 try:
     from db.base import Base
@@ -10,6 +11,46 @@ class UserModel(Base):
     __tablename__ = 'users'
 
     id = Column('id', String, primary_key=True, nullable=False)
+    name = Column('name', String, nullable=False)
     email = Column('email', String, nullable=False, unique=True)
     password = Column('password', String, nullable=False)
+    phone = Column('phone', String, default='')
+    photo = Column('photo', LargeBinary, default='')
+    isUpdated = Column('isUpdated', Boolean, default=0)
     emailVerified = Column('emailVerified', Boolean, default=0)
+
+    # Relacionamentos
+    address = relationship(
+        "UserAddressModel", back_populates="user", uselist=False)
+    identification = relationship(
+        "UserIdentificationModel", back_populates="user", uselist=False)
+
+
+class UserAddressModel(Base):
+    __tablename__ = 'userAddress'
+
+    id = Column('id', String, primary_key=True, nullable=False)
+    user_id = Column('user_id', String, ForeignKey('users.id'), unique=True)
+    country = Column('country', String)
+    zipCode = Column('zipCode', String)
+    street = Column('street', String)
+    number = Column('number', Integer)
+    complement = Column('complement', String)
+    city = Column('city', String)
+    state = Column('state', String)
+
+    # Define o relacionamento com UserModel
+    user = relationship("UserModel", back_populates="address")
+
+
+class UserIdentificationModel(Base):
+    __tablename__ = 'UserIdentification'
+
+    id = Column('id', String, primary_key=True)
+    user_id = Column('user_id', String, ForeignKey('users.id'), unique=True)
+    nacionality = Column('nacionality', String)
+    document = Column('document', String)
+    birthDate = Column('birthDate', String)
+
+    # Define o relacionamento com UserModel
+    user = relationship("UserModel", back_populates="identification")
