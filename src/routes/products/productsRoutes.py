0dd474a17f995f6bb87c43eabe4d3e06a9dfcc6a
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-09-20 23:45:10 -0300

    arrumando update e get de imagem

--- src/routes/products/productsRoutes.py
+++ src/routes/products/productsRoutes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile
+import os
+
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi.responses import FileResponse
+from PIL import Image
 from sqlalchemy.orm import Session
 
 from app.controllers.productController import productController
@@ -6,8 +10,6 @@ from app.middlewares.verifyJWT import verifyJWT
 from app.schemas.productSchema import Product
 from db.models import UserModel
 from lib.depends import get_db_Session
-import os
-from PIL import Image
 
 productRoutes = APIRouter()
 
@@ -27,14 +29,14 @@ def create(
         return e
 
 
-@productRoutes.get("/{id}", summary="Busca um produto pelo id.")
+@productRoutes.get("/{product_id}", summary="Busca um produto pelo id.")
 def getById(
-    id: str, user: UserModel = Depends(verifyJWT), db: Session = Depends(get_db_Session)
+    product_id: str, user: UserModel = Depends(verifyJWT), db: Session = Depends(get_db_Session)
 ):
     product_controller = productController(db)
 
     try:
-        return product_controller.findProductById(id)
+        return product_controller.findProductById(product_id)
     except Exception as e:
         print(e)
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
@@ -51,67 +53,89 @@ def getAll(user: UserModel = Depends(verifyJWT), db: Session = Depends(get_db_Se
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
 
 
-@productRoutes.put("/update/{id}", summary="Atualiza informações do produto")
+@productRoutes.put("/update/{product_id}", summary="Atualiza informações do produto")
 def update(
-    id: str,
+    product_id: str,
     data: Product,
-    user: UserModel = Depends(verifyJWT),
     db: Session = Depends(get_db_Session),
 ):
     product_controller = productController(db)
 
     try:
-        return product_controller.update(id, data)
+        return product_controller.update(product_id, data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) from e
 
 
-@productRoutes.delete("", summary="Delete um produto.")
+@productRoutes.delete("/{product_id}", summary="Delete um produto.")
 def delete(
-    id: str, user: UserModel = Depends(verifyJWT), db: Session = Depends(get_db_Session)
+    product_id: str, user: UserModel = Depends(verifyJWT), db: Session = Depends(get_db_Session)
 ):
     product_controller = productController(db, user)
 
     try:
-        return product_controller.deleteProduct(id)
+        return product_controller.deleteProduct(product_id)
     except Exception as e:
         print(e)
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
 
 
-@productRoutes.post("/upload-image/{id}", summary="Upload imagem do produto.")
-def upload_product_image(id: str, file: UploadFile = File(...)):
+
+
+@productRoutes.post("/image/{product_id}", summary="Upload imagem do produto.")
+def upload_product_image(product_id: str, file: UploadFile = File(...)):
     try:
-        cwd = os.getcwdb()
-        path_image_dir = "upload-images/product/image" + id + "/"
-        full_image_path = os.path.join(cwd, path_image_dir, file.filename)
+        base_dir = "images/product/"
+        id_str = str(product_id)
+        path_image_dir = os.path.join(base_dir, id_str)
+        
+        # todo: verificar se o arquivo é uma imagem (png, jpg, jpeg)
+        if file.content_type not in ["image/png", "image/jpg", "image/jpeg"]:
+            raise Exception("O arquivo deve ser uma imagem.")
+        
+        #  todo: armazenar a extensão do arquivo
+        ext = file.content_type.split("/")[1]
+        
+        # todo: mudar extensão do arquivo para png
+        file.filename = "productPhoto.png"
+        
+        # todo: verificar se o arquivo é maior que 2mb
+        if file.size > 2097152:
+            raise Exception("O arquivo deve ser menor que 2mb.")
+        
+        #  todo: verificar se ja existe uma imagem para o produto e apagar
+        if os.path.exists(os.path.join(path_image_dir, "productPhoto.png")):
+            os.remove(os.path.join(path_image_dir, "productPhoto.png"))        
 
-        if not os.path.exists(path_image_dir):
-            os.mkdir(path_image_dir)
+        # Crie o diretório e todos os subdiretórios necessários, se não existirem
+        os.makedirs(path_image_dir, exist_ok=True)
 
-        file_name = full_image_path.replace(file.filename, "product.png")
+        full_image_path = os.path.join(path_image_dir, "productPhoto.png")
 
-        with open(file_name, "wb+") as f:
+        with open(full_image_path, "wb+") as f:
             f.write(file.file.read())
             f.flush()
-            f.close()
 
-        return {"product_image": os.path.join(path_image_dir, "product.png")}
+        # Use FileResponse para retornar o arquivo
+        return FileResponse(full_image_path, media_type=f'image/png')
     except Exception as e:
         print(e)
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-@productRoutes.get("/upload-image/{id}", summary="Get imagem do produto.")
-def get_product_image(id: str):
+
+
+@productRoutes.get("/image/{product_id}", summary="Get imagem do produto.")
+def get_product_image(product_id: str):
     try:
         cwd = os.getcwdb()
-        path_image_dir = "upload-images/product/image" + id + "/"
-        full_image_path = os.path.join(cwd, path_image_dir, "product.png")
+        base_dir = "images/product/"
+        id_str = str(product_id)
+        path_image_dir = os.path.join(base_dir, id_str)
+                        
+        full_image_path = os.path.join(cwd.decode('utf-8'), path_image_dir, "productPhoto.png")
 
-        if os.path.exists(full_image_path):
-            image = Image.open(full_image_path)
-            image.thumbnail((400, 400), Image.ANTIALIAS)
-
-        return {"product_image": os.path.join(path_image_dir, "product_400x400.png")}
+        return FileResponse(full_image_path, media_type='image/png')
     except Exception as e:
         print(e)
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
