671456547408f6490171f8a85947979a5d8e9cfc
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2023-10-31 08:40:04 -0300

    autalizando retorno da rota de produtos a venda no marketplace

--- src/app/services/offer/findMarketplaceOffers.py
+++ src/app/services/offer/findMarketplaceOffers.py
@@ -1,18 +1,27 @@
 from app.repositories.offer.offerRepository import OfferRepository
+from app.repositories.products.productRepository import ProductRepository
 
 
 class FindMarketplaceOffers:
     def __init__(self, db):
         self.db = db
         self.offer_repository = OfferRepository(db)
+        self.product_repository = ProductRepository(db)
     
     def execute(self, page, take):
         offers = self.offer_repository.findMarketplaceOffers(page, take)
         
+        for offer in offers:
+            product = self.product_repository.find_by_id(offer.product_id)
+            offer.product = product
+            
+        pageCount = self.offer_repository.countMarketplaceOffers()
+        
         json = {
             "offers": offers,
+            "take": take,
             "page": page,
-            "take": take
+            "pageCount": pageCount
         }
         
         return json
