c71d2b8c5328cb486c59cd6506a285884b211038
cDanx <daniel25agm@hotmail.com>
2023-11-03 00:36:19 -0300

    Modelagem tabela: Wallets, Transactions, payMethod

--- src/db/models.py
+++ src/db/models.py
@@ -123,7 +123,7 @@ class paymentMethodModel(Base):
 
     id = Column(
         "id", Integer, primary_key=True, nullable=False, default=0
-    )  # 1 cartão, 2 PIX
+    )  # 1 cartão, 2 PIX,
     method = Column("method", String, nullable=False)
 
 
@@ -131,6 +131,12 @@ class WalletsModel(Base):
     __tablename__ = "wallets"
 
     id = Column("id", String, primary_key=True, nullable=False)
+    amount_recluse = Column(
+        "amount_recluse", Numeric(precision=10, scale=2), nullable=False, default=0
+    )
+    amount_free = Column(
+        "amount_free", Numeric(precision=10, scale=2), nullable=False, default=0
+    )
 
     user_id = Column("user_id", String, ForeignKey("users.id"))
 
@@ -141,33 +147,21 @@ class TransactionsModel(Base):
     __tablename__ = "transactions"
 
     id = Column("id", String, primary_key=True, nullable=False)
-
-    userBuyer_phoneNumber = Column("userBuyer_phoneNumber", String, default="")
-
-    userBuyer_name = Column("userBuyer_name", String, nullable=False)
-
-    userBuyer_mail = Column("userBuyer_mail", String, nullable=False, unique=True)
-
-    wallet_id = Column("wallet_id", String, ForeignKey("wallets.id"))
-
-    product_id = Column("product_id", String, ForeignKey("products.id"))
-
     offer_id = Column("offer_id", String, ForeignKey("offers.id"))
-
     price = Column("price", Numeric(precision=10, scale=2), nullable=False)
+    situation = Column("situation", Integer, nullable=False, default=1)
+    transactionDate = Column("transactionDate", String, nullable=False)
+    aproveDate = Column("aproveDate", String, nullable=False)
+    reimbursementDate = Column("reimbursementDate", String, default="")
 
+    buyer_id = Column("buyer_id", String, ForeignKey("users.id"))
+    wallet_id = Column("wallet_id", String, ForeignKey("wallets.id"))
+    product_id = Column("product_id", String, ForeignKey("products.id"))
     paymentMethod_id = Column(
         "paymentMethod_id", Integer, ForeignKey("paymentMethod.id")
     )
 
-    situation = Column("situation", Integer, nullable=False, default=1)
-
-    transactionDate = Column("transactionDate", String, nullable=False)
-
-    aproveDate = Column("aproveDate", String, nullable=False)
-
-    reimbursementDate = Column("reimbursementDate", String, default="")
-
+    user = relationship("UserModel", back_populates="transactions")
     wallet = relationship("WalletsModel", back_populates="transactions")
     paymentMethod = relationship("paymentMethodModel", back_populates="transactions")
     offer = relationship("OfferModel", back_populates="transactions")
