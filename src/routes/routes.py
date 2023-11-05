7e504fc7eee24961027f7adddd640a3d9870f7b8
cDanx <daniel25agm@hotmail.com>
2023-11-05 13:15:42 -0300

    Modularizacao e Create Transacao, Create Wallet

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from routes.products.productsRoutes import productRoutes
 from routes.user.userRoutes import userRoutes
 from routes.offer.offerRoutes import offerRoutes
+from routes.transactions.transactionsRoutes import transactionsRoutes
 
 routes = APIRouter()
 
 routes.include_router(userRoutes, prefix="/user", tags=["Usuario"])
 routes.include_router(productRoutes, prefix="/product", tags=["Produto"])
 routes.include_router(offerRoutes, prefix="/offer", tags=["Oferta"])
+routes.include_router(transactionsRoutes, prefix="/transacao")
